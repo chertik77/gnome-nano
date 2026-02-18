5da70e4a34af2834ee365b4a29c5fe6cab1f3d6f
Denys Babych <129002577+chertik77@users.noreply.github.com>
2026-02-18 13:42:10 +0000

    Update setup.sh to refresh nano configuration
    Remove existing nano configuration files and install new nanorc.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
+rm -rf ~/.nanorc
+rm -rf ~/.nano
+
 wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
 
 echo "
-  set linenumbers
-  set autoindent
-  set zero
-  set tabsize 2
-  set tabtospaces
-  set softwrap
-  set saveonexit
-  set numbercolor magenta
-  set titlecolor white
+set linenumbers
+set autoindent
+set zero
+set tabsize 2
+set tabtospaces
+set softwrap
+set saveonexit
+set numbercolor magenta
+set titlecolor white
 
-  include '~/.nano/nanorc/python.nanorc'
-  include '~/.nano/nanorc/sh.nanorc'
+include '~/.nano/nanorc/python.nanorc'
+include '~/.nano/nanorc/sh.nanorc'
 " >> ~/.nanorc
