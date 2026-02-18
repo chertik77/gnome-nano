a8d03856d9970c1ab95134cb291fd1215b380eac
Denys Babych <129002577+chertik77@users.noreply.github.com>
2026-02-18 13:34:27 +0000

    fix: replace ls command with nanorc installation script
    Install nanorc configuration for nano editor.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,16 @@
-ls -l
+wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
+
+echo "
+  set linenumbers
+  set autoindent
+  set zero
+  set tabsize 2
+  set tabtospaces
+  set softwrap
+  set saveonexit
+  set numbercolor magenta
+  set titlecolor white
+
+  include '~/.nano/nanorc/python.nanorc'
+  include '~/.nano/nanorc/sh.nanorc'
+" >> ~/.nanorc
