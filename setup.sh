9a99a01b71a9726fc84ab36a07367d2dc32f05ab
Denys Babych <129002577+chertik77@users.noreply.github.com>
2026-03-06 11:56:24 +0000

    refactor: setup.sh for aliases and keybindings
    Updated setup script to add multiple aliases and modern keybindings.

--- setup.sh
+++ setup.sh
@@ -1,24 +1,11 @@
 rm -rf ~/.nanorc
 rm -rf ~/.nano
 
-if ! grep -q "alias n=nano" ~/.bashrc; then
-  echo 'alias n=nano' >> ~/.bashrc
-fi
+for a in "alias n=nano" "alias p=python3"; do
+  grep -qxF "$a" ~/.bashrc || echo "$a" >> ~/.bashrc
+done
 
 git clone https://github.com/galenguyer/nano-syntax-highlighting.git ~/.nano/nanorc
-
-echo
-read -p "Do you want modern keybindings (Alt+z undo, ALt+Z redo, Alt+/ comment)? (y/n): " keys
-
-if [[ "$keys" == "y" || "$keys" == "Y" ]]; then
-	KEYBINDS='
-bind M-z undo main
-bind M-Z redo main
-bind M-/ comment main
-'
-else
-  KEYBINDS=""
-fi
 
 cat <<EOF > ~/.nanorc
 set linenumbers
@@ -29,8 +16,11 @@ set tabsize 2
 set softwrap
 set saveonexit
 set numbercolor magenta
-set titlecolor white
-$KEYBINDS
+
+bind M-z undo main
+bind M-Z redo main
+bind M-/ comment main
+
 include ~/.nano/nanorc/*.nanorc
 EOF
 
