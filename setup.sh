fc439c7f5186ecce744025b776896a8b87145bf7
Denys Babych <129002577+chertik77@users.noreply.github.com>
2026-03-06 13:56:40 +0000

    feat: update setup.sh to include GNOME Terminal color scheme
    Add Catppuccin Mocha color scheme setup for GNOME Terminal.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ for a in "alias n=nano" "alias p=python3"; do
   grep -qxF "$a" ~/.bashrc || echo "$a" >> ~/.bashrc
 done
 
+curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | p - 
+# Sets a default profile color scheme to Catppuccin Mocha 
+gsettings set org.gnome.Terminal.ProfilesList default 95894cfd-82f7-430d-af6e-84d168bc34f5
 
 git clone https://github.com/galenguyer/nano-syntax-highlighting.git ~/.nano/nanorc
 
